ebd311a55d8d3b67e16c65ea93cf20321ddb8184
ETHAN ANDERSON (CRGL-THIRDPARTY.COM) <ETHAN_ANDERSON@CRGL-THIRDPARTY.COM>
2017-04-12 11:04:07 -0500

    init

---
+++ src/main/java/com/ethanaa/lambdachan/LambdachanApplication.java
@@ -0,0 +1,12 @@
+package com.ethanaa.lambdachan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdachanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdachanApplication.class, args);
+	}
+}
